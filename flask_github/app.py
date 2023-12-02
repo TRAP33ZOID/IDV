8e586f532168417073d2c25fe2a10e48e8e78932
TRAP33ZOID <wamm99@gmail.com>
2023-12-02 14:24:12 +0300

    implemented a sign in page

--- flask_github/app.py
+++ flask_github/app.py
@@ -1,11 +1,73 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for, session
 import fitz  # PyMuPDF
 import openai
 import os
-import tempfile
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'C:\\Users\\Waleed\\Desktop\\website dev\\pdf'  # replace with your desired folder
+ALLOWED_EXTENSIONS = {'pdf'}  # allowed file types
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB max upload size
+app.secret_key = 'your_very_secret_key_here'  # Replace with your secret key
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'admin':
+            session['logged_in'] = True
+            #flash('You are now logged in.', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Wrong login credentials', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/')
+def index():
+    app.logger.debug(f"Logged in? {'logged_in' in session}")
+    if not session.get('logged_in'):
+        #flash('Please log in to access this page.', 'warning')
+        return redirect(url_for('login'))
+    return render_template('index.html')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    if not session.get('logged_in'):
+        return jsonify({"error": "Unauthorized"}), 401
+
+    file = request.files['file']
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        # Check if file exists to avoid overwriting
+        counter = 1
+        while os.path.exists(save_path):
+            name, extension = os.path.splitext(filename)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{name}_{counter}{extension}")
+            counter += 1
+
+        file.save(save_path)
+
+        extracted_text = extract_text_pymupdf(save_path)
+        summary = summarize_text(extracted_text)
+        os.unlink(save_path)  # Remove the file if you don't want to keep it after processing
+        return jsonify({"summary": summary, "extractedText": extracted_text[:500]})
+    else:
+        return jsonify({"error": "Invalid file type or no file uploaded"}), 400
 
 def extract_text_pymupdf(pdf_file_path):
     doc = fitz.open(pdf_file_path)
@@ -16,60 +78,15 @@ def extract_text_pymupdf(pdf_file_path):
     return text
 
 def summarize_text(text):
-    openai.api_key = 'enter api key here'
+    openai.api_key = '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'
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-1106",
         messages=[
-            {"role": "system", "content": '''Summarize the following text fron an insurance policy:
-             try to take the following into consideration
-                 Type of Insurance
-        Question: "Can you identify and describe the type of insurance covered in this policy? For example, is it Home, Auto, or Life insurance, and what are the key characteristics of this type?"
-
-    Category of Coverage
-        Question: "What are the different categories of coverage included in this policy? Please describe each category, such as Dwelling, Personal Property, or Liability, and their specific coverage details."
-
-    What's Covered
-        Question: "Could you list and elaborate on the specific items, scenarios, or risks that are covered under this policy? Please include detailed information like coverage limits, conditions, and any specific terms mentioned."
-
-    What's Not Covered
-        Question: "What are the exclusions and limitations of this policy? I'm interested in knowing what scenarios, items, or risks are specifically not covered, along with any exceptions or additional text that provides context to these exclusions."
-
-    Events Covered
-        Question: "Which events or circumstances does this policy cover? Please provide a comprehensive list of events like Fire, Theft, or Natural Disasters, and include any clarifications or exceptions associated with these covered events."
-
-    Additional Details
-        Question: "Are there any additional details, hover-over texts, or clarifications provided in the policy that are important to understand its coverage, exclusions, or events covered? If so, please provide these details for a more comprehensive understanding of the policy."''' },
+            {"role": "system", "content": "Please summarize this text considering the following points: Type of Insurance, Category of Coverage, What's Covered, What's Not Covered, Events Covered, and Additional Details."},
             {"role": "user", "content": text}
         ]
     )
     return response['choices'][0]['message']['content']
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/summarize', methods=['POST'])
-def summarize():
-    try:
-        file = request.files['file']
-        if file and file.filename.endswith('.pdf'):
-            # Save the file stream to a temporary file
-            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                file.save(temp_file.name)
-                temp_file_path = temp_file.name
-
-            # Process the PDF file
-            extracted_text = extract_text_pymupdf(temp_file_path)
-
-            # Summarize the text
-            summary = summarize_text(extracted_text)
-            os.unlink(temp_file_path)  # Remove the temporary file
-            return jsonify({"summary": summary, "extractedText": extracted_text[:500]})
-        else:
-            return jsonify({"error": "Invalid file type or no file uploaded"}), 400
-    except Exception as e:
-        app.logger.error(f'Error: {str(e)}')
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
